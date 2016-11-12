d74d3f9aad4ee2f951997f7059539df073e90f76
Fran Saavedra <fran14.95@gmail.com>
2016-11-12 19:36:52 +0100

    Estilos

--- Worksheet 2/ejercicio1.js
+++ Worksheet 2/ejercicio1.js
@@ -1,4 +1,52 @@
-function showAlert() {
-    alert("click");
+function normal() {
+    eliminarEstilos(document.body);
 }
-document.addEventListener("click", showAlert);
+
+function eliminarEstilos(elem) {
+    elem.removeAttribute("style");
+
+    for(var child in elem.childNodes) 
+        if(elem.childNodes[child].nodeType == 1)
+            eliminarEstilos(elem.childNodes[child]);
+}
+
+
+function minimalista() {
+    var lateral = document.getElementById("lateral");
+    lateral.style.float = "left";
+    lateral.style.width = "10%";
+    lateral.style.height = "100%";
+    lateral.style.backgroundColor = "#2196F3";
+
+    var central = document.getElementById("central");
+    central.style.float = "right";
+    central.style.width = "90%";
+    central.style.height = "100%";
+    central.style.backgroundColor = "#FFF9C4";
+
+    var menu = document.getElementById("menu");
+    menu.style.margin = "15px";
+    menu.style.backgroundColor = "#F5F5F5";
+
+    var header = document.getElementById("header");
+    header.style.margin = "15px";
+
+    var footer = document.getElementById("footer");
+    footer.style.border = "1px solid #000000";
+    footer.style.backgroundColor = "#FFEB3B";
+
+    var arrayP = document.getElementsByTagName("p");
+    for(var i = 0; i < arrayP.length; i++) 
+        arrayP.item(i).style.margin = "15px";
+}
+
+window.onload = function () {
+    document.getElementById("normal").addEventListener("click",function (e) {
+        e.preventDefault();
+        normal();
+    });
+    document.getElementById("minimalista").addEventListener("click",function (e) {
+        e.preventDefault();
+        minimalista();
+    });
+};
--- Worksheet 2/ejercicio2.js
+++ Worksheet 2/ejercicio2.js
@@ -1,5 +1,31 @@
-function getPosition(event) {
-	var div = document.getElementById("div");
-    div.innerHTML= "X: " + event.clientX + ", Y: " + event.clientY;
+function normal() {
+   cambiarEstilo("normal.css");
 }
-document.onmousemove = function (event){getPosition(event)};
+
+function minimalista() {
+    cambiarEstilo("minimalista.css");
+}
+
+
+function cambiarEstilo(archivo) {
+
+    var oldLink = document.getElementsByTagName("link").item(0);
+
+    var newLink = document.createElement("link");
+    newLink.setAttribute("rel", "stylesheet");
+    newLink.setAttribute("type", "text/css");
+    newLink.setAttribute("href", archivo);
+
+    document.getElementsByTagName("head").item(0).replaceChild(newLink, oldLink);
+}
+
+window.onload = function () {
+    document.getElementById("normal").addEventListener("click",function (e) {
+        e.preventDefault();
+        normal();
+    });
+    document.getElementById("minimalista").addEventListener("click",function (e) {
+        e.preventDefault();
+        minimalista();
+    });
+};
